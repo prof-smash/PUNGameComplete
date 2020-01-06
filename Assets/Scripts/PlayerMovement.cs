292c1c6dce41895b2334da84cc31802c90f499fc
prof-smash <talbert31095@gmail.com>
2020-01-06 10:35:26 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+            Movement();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    void Movement()
+    {
+        float yMov = Input.GetAxis("Vertical");
+        float xMov = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(xMov / 7.5f, yMov / 7.5f, 0);
+    }
+}
